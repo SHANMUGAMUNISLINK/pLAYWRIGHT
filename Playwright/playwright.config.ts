d48c0fb4edd6dc49606bab23a475bc5ba0545561
shanmugarajeshwaran.m@unislink.com <shanmugarajeshwaran.m@unislink.com>
2023-09-22 16:11:56 +0530

    New update

--- Playwright/playwright.config.ts
+++ Playwright/playwright.config.ts
@@ -34,7 +34,10 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
  // reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+
+  
   use: {
+ 
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
     baseURL: "https://opensource-demo.orangehrmlive.com",
@@ -42,6 +45,7 @@ export default defineConfig({
      video: {
       mode: 'on-first-retry',
       size: { width: 640, height: 480 },
+      
     },
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
--- Playwright/tests/Rcm.spec.ts
+++ Playwright/tests/Rcm.spec.ts
@@ -1,41 +1,45 @@
 import { test, expect } from '@playwright/test';
+import puppeteer from 'puppeteer';
 
-
-test('Login to rcm', async ({ page }) => {
-  await page.goto('https://beta.unislink.com/rcm/');
-  await page.getByRole('button', { name: 'LOGIN WITH AD' }).click();
-  await page.locator('[type="email"]').fill('shanmugarajeshwaran.m@unislink.com');
-  await page.getByRole('button', { name: 'Next' }).click();
-  await page.getByPlaceholder('Password').fill('Dur57272');
-  await page.getByRole('button', { name: 'Sign in' }).click();
-  await page.getByRole('button', { name: 'Yes' }).click();
-  await page.waitForLoadState('load');
+test('Login to rcm', async ({ browser }) => {
+  // const pupp = await puppeteer.launch({headless: false});
+  // const pup = await pupp.newPage();
+  const context = await browser.newContext({
+    storageState:"./login.json"
+  })
+  const page = await context.newPage();
+  //URL
+  await page.goto('https://beta.unislink.com/rcm/', {waitUntil: 'load'});
+  //login setup
+  // await page.getByRole('button', { name: 'LOGIN WITH AD' }).click();
+  // await page.locator('[type="email"]').fill('shanmugarajeshwaran.m@unislink.com');
+  // await page.getByRole('button', { name: 'Next' }).click();
+  // await page.getByPlaceholder('Password').fill('Dur57272');
+  // await page.getByRole('button', { name: 'Sign in' }).click();
+  // await page.getByRole('button', { name: 'Yes' }).click();
+  // await page.waitForLoadState('load');
+ //RCM page
   await page.getByRole('link', { name: 'Enterprise Reporting' }).click();
   const link = page.getByRole('link', { name: 'Automation' });
   expect(await link.isVisible()).toBe(true);
   await link.click();
+  //it click bot
   await page.getByRole('link', { name: 'Bots' }).click();
   await page.waitForLoadState('load');
-  const div = page.locator('a#bot-dashboard');
-  await div.click();
+ //it click bot dashboard
+  const div = page.getByRole('link', { name: 'Bot Dashboard' }).click();
+  await page.screenshot({path:'Screenshot/bot.png'})
   await page.waitForLoadState('load');
+ //fullpage
+  const fullpage = page.locator('rpabotdashboard');
   
-  const dash = page.getByRole('link', { name: 'Bot Dashboard' });
-  await dash.screenshot({path:'full.png'});
-  const element = page.locator('div:nth-of-type(1) > .b.bg-white.p-2');
-  await page.waitForTimeout(7000);
-  try {
-     const collection = page.locator('div:nth-of-type(1) > .b.bg-white.p-2');
-    await page.waitForSelector('element', { state: 'visible' });
-  
-   
-    await collection.screenshot({ path: 'element_screenshot.png' });
-  } catch (error) {
-   
-    console.error(`Error: ${error.message}`);
-    await page.screenshot({ path: 'error_screenshot.png' });
-  }
-  
-  
-
+  await fullpage.screenshot({path: 'Screenshot/full.png'});
+  //collection
+  const collection = await page.locator('div:nth-of-type(1) > .b.bg-white.p-2');
+  await collection.screenshot({path:'Screenshot/collection.png'})
+  //1-cent
+  const Adjustment = await page.locator('div:nth-of-type(2) > .b.bg-white.p-2');
+  await page.waitForSelector('div:nth-of-type(2) > .b.bg-white.p-2'); 
+  await Adjustment.screenshot({path:'Screenshot/1-Cent Adjustment.png'});
+ 
 });
