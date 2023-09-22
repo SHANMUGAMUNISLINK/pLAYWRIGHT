e4144d84a5b6b23b7159de3351fd43e6a69439e7
shanmugarajeshwaran.m@unislink.com <shanmugarajeshwaran.m@unislink.com>
2023-09-22 22:40:00 +0530

    updated

--- Playwright/tests/Rcm-beta.spec.ts
+++ Playwright/tests/Rcm-beta.spec.ts
@@ -7,26 +7,42 @@ import dotenv from 'dotenv';
 
 test('login to beta', async ({ page }) => {
   
-  await page.goto('https://beta.unislink.com/rpt/');
+  await page.goto('https://beta.unislink.com/rpt/',{waitUntil: 'load'});
 
  
   await page.getByRole('link', { name: 'Unislink AD' }).click();
 
-  
-  await page.locator('[type="email"]').fill(process.env.USERNAME);
-
+  await page.locator('[type="email"]').fill('shanmugarajeshwaran.m@unislink.com');
+  //await page.locator('[type="email"]').fill(process.env.USERNAME);
+//    await page.waitForTimeout(4000);
  
   await page.getByRole('button', { name: 'Next' }).click();
   
+  await page.getByPlaceholder('Password').fill('Dur57272');
+ // await page.getByPlaceholder('Password').fill(process.env.PASSWORD);
+ await page.getByRole('button', { name: 'Sign in' }).click();
+ await page.getByRole('button', { name: 'Yes' }).click();
+ await page.locator('.icon-notebook').click();
+ const locator = await page.locator('.font-12.p-2.text-dark.text-right > i'); 
+const tex = await locator.textContent(); 
+console.log("it show date:"+tex);
 
-  await page.getByPlaceholder('Password').fill(process.env.PASSWORD);
+const string = await page.locator('.ng-star-inserted.pointer.text-primary');
+await string.click();
+const pass= await string.textContent();
+console.log("it show hospital name:"+pass);
+ 
+//await page.locator('.ng-arrow-wrapper').click();
+//await page.locator('.ng-arrow-wrapper').click();
+// const drop =await
+//await page.locator('div[role="combobox"] > input[type="text"]').click();
+// await drop.click();
+//await drop.selectOption({value:'LAKESHORE OPEN MRI'})
+
+await page.locator('.ng-arrow-wrapper').selectOption({ index: 2 });
+//  await page.locator('.ng-arrow-wrapper').click();
+//  await page.locator('.ng-arrow-wrapper').selectText()
+ await page.waitForTimeout(6000);
 
  
-  await page.getByRole('button', { name: 'Sign in' }).click();
-
-  
-  const string = await page.locator('.icon-notebook').click();
-
-
-  console.log(string);
 });
